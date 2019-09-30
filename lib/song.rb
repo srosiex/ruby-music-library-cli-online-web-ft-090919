de79c913cb346d19f39ee2d246a30059773a726a
Sarah H <sarahrose.henderson@gmail.com>
2019-09-30 15:07:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ attr_accessor :name, :artist, :genre
 
   def genre=(genre)
     @genre = genre
-    genre.songs << unless genre.songs.include?(self)
+    genre.songs << self  unless genre.songs.include?(self)
   end
-
+end
 end
