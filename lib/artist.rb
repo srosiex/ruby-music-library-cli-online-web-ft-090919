ca6cba3521477f4a8394fe56f664e3e8801c3f7e
Sarah H <sarahrose.henderson@gmail.com>
2019-09-30 15:19:06 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,8 @@
 class Artist
-  attr_accessor :name, :songs
+  attr_accessor :name, :song, :musiclibrarycontroller, :musicimporter
+  extend Concerns::Findable
   @@all = []
+
   def initialize(name)
     @name = name
     @songs = []
@@ -9,6 +11,7 @@ class Artist
   def self.all
     @@all
   end
+
   def self.destroy_all
     @@all.clear
   end
@@ -23,28 +26,34 @@ class Artist
     artist
   end
 
-  def add_song(song)
-    if song.artist == nil
-      song.artist = self
-      @songs << song
-  end
-end
-
   def songs
     @songs
   end
 
-  def genres
-    @new_array = []
-      @songs.each do |song|
-        if @new_array.include?(song.genre)
-          nil
-        else
-          @new_array << song.genre
-        end
-      end
-      @new_array
+  def add_song(song)
+    if song.artist == nil
+      song.artist = self
+    else
+      nil
+    end
+    if @songs.include?(song)
+      nil
+    else
+      @songs << song
+    end
+    song
   end
 
+  def genres
+    @new_array = []
+    @songs.each do |song|
+      if @new_array.include?(song.genre)
+        nil
+      else
+        @new_array << song.genre
+      end
+    end
+    @new_array
+  end
 
 end
