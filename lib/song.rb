01e64d23fca001f3257c76cc55f81ac34518b4eb
Sarah H <sarahrose.henderson@gmail.com>
2019-09-30 15:19:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,41 +1,84 @@
 class Song
-attr_accessor :name, :artist, :genre
-# attr_reader :artist
+  attr_accessor :name, :artist, :genre, :musicimporter, :musiclibrarycontroller
+  extend Concerns::Findable
+
   @@all = []
 
   def initialize(name, artist=nil, genre=nil)
     @name = name
-    @@all << self
-    self.artist=(artist) unless artist == nil
-    self.genre=(genre) unless genre == nil
-
+    self.artist=(artist) if artist != nil
+    self.genre=(genre) if genre != nil
   end
+
   def self.all
     @@all
   end
+
   def self.destroy_all
     @@all.clear
   end
+
   def save
     @@all << self
   end
-  def self.create(song, artist=nil, genre=nil)
-    song = Song.new(song, artist, genre)
+
+  def self.create(song)
+    song = self.new(song)
     song.save
     song
   end
+
+  def artist
+    @artist
+  end
+
   def artist=(artist)
     @artist = artist
-  artist.add_song(self)
+    artist.add_song(self)
+  end
+
+  def genre
+    @genre
   end
 
   def genre=(genre)
     @genre = genre
-    genre.songs << self  unless genre.songs.include?(self)
+    genre.songs << self unless genre.songs.include?(self)
   end
-  def genres
-    @genre
+
+  def self.find_by_name(name)
+    @@all.detect do |song|
+      song.name == name
+    end
   end
-  
+
+  def self.find_or_create_by_name(name)
+    # @@all.detect do |song|
+    #   if song.name == name
+    #     song
+    #   else
+    #     self.create(name)
+    #   end
+    # end
+    self.find_by_name(name) || self.create(name)
+
+  end
+
+  def self.new_from_filename(filename)
+    array = filename.split(" - ")
+
+    song_name = array[1]
+    artist_name = array[0]
+    genre_name = array[2].split(".mp3").join
+
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+    self.new(song_name, artist, genre)
+  end
+
+  def self.create_from_filename(filename)
+    self.new_from_filename(filename).save
+  end
+
 
 end
