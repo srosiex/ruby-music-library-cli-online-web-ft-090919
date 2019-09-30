41bc9be33c9994d45db95b1ff43e4a1bcc10a1a0
Sarah H <sarahrose.henderson@gmail.com>
2019-09-30 15:21:42 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -7,15 +7,19 @@ attr_reader :songs
    @name = name
    @songs = []
  end
+
  def self.all
    @@all
  end
+
  def self.destroy_all
    @@all.clear
  end
+
  def save
-   @@all << self
+   self.class.all << self
  end
+
  def self.create(name)
    genre = Genre.new(name)
    genre.save
