f99082cf9d1b5b845c05a2daf7b402c69856d077
Sarah H <sarahrose.henderson@gmail.com>
2019-09-30 15:24:19 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -47,5 +47,9 @@ class Genre
       song.artist = self unless song.artist
       songs << song unless songs.include?(song)
     end
+    def genres
+   songs.collect{ |s| s.genre }.uniq
+ end
+
 
 end
